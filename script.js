997c29c1a9b3c4fbb89d1fdce22fd71cc641d4c8
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-06-18 09:05:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+$(function() {
+  // Make the divs draggable
+  $(".draggable").draggable({
+    containment: "body",
+    revert: "invalid",
+    cursor: "move"
+  });
+
+  // Make the divs droppable
+  $(".draggable").droppable({
+    drop: function(event, ui) {
+      var draggableId = ui.draggable.attr("id");
+      var droppableId = $(this).attr("id");
+      
+      // Swap background images
+      var draggableBg = ui.draggable.css("background-image");
+      var droppableBg = $(this).css("background-image");
+      
+      ui.draggable.css("background-image", droppableBg);
+      $(this).css("background-image", draggableBg);
+    }
+  });
+});
